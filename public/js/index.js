8507a63fa87c7831555d29b1a31e26517901ab41
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2024-04-13 00:34:36 +0200

    docs layout header styles

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
 import '@dile/ui/components/card/card';
-
+import '@dile/ui/components/menu-hamburger/menu-hamburger';
+import '@dile/ui/components/nav/nav';
+import '@dile/ui/components/social-icon/social-icon';
 console.log('hola');
