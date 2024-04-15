caf6fdb078eb151513d235f8cacca1c625f09b9b
Miguel Angel Alvarez <miguel@desarrolloweb.com>
2024-04-15 20:15:37 +0200

    dark icon option on dile-avatar

--- packages/ui/components/avatar/src/DileAvatar.js
+++ packages/ui/components/avatar/src/DileAvatar.js
@@ -1,7 +1,5 @@
 import { LitElement, html, css } from "lit";
 
-let defaultIcon = `data:image/svg+xml;utf8,<svg style="fill:%23888;" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"/><path d="M0 0h24v24H0z" fill="none"/></svg>`;
-
 export class DileAvatar extends LitElement {
   static get styles() {
     return css`
@@ -30,6 +28,7 @@ export class DileAvatar extends LitElement {
     return {
       src: { type: String },
       initial: { type: String },
+      darkIcon: { type: Boolean },
     };
   }
   constructor() {
@@ -57,7 +56,7 @@ export class DileAvatar extends LitElement {
     return html`<div
         id="img"
         role="img"
-        style="background-image: url('${this._asignImage(this.src)}');"
+        style="background-image: url('${this._asignImage(this.src, this.darkIcon)}');"
       >&nbsp;</div>`;
   }
 
@@ -69,10 +68,14 @@ export class DileAvatar extends LitElement {
       >${this.initial}</div>`;
   }
 
-  _asignImage(src) {
+  _asignImage(src, isDark) {
     if (src == "" || !src) {
-      return defaultIcon;
+      return this.defaultIcon(isDark);
     }
     return src;
+  }
+
+  defaultIcon(isDark) {
+    return `data:image/svg+xml;utf8,<svg style="fill:%23${isDark ? '303030' : 'fff'};" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"/><path d="M0 0h24v24H0z" fill="none"/></svg>`;
   }
 }
